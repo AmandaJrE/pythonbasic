5c82b47773a3f4c78cdf2d1a4277246e3d562690
Amanda Andrade <96007035+amandaJrU@users.noreply.github.com>
2022-08-16 14:26:31 -0300

    fixPython

---
+++ cresc.py
@@ -0,0 +1,8 @@
+a = int(input('Escreva um número: '))
+b = int(input('Escreva outro número: ')) 
+c = int(input('Escreva mais um número: ')) 
+
+if (a < b < c):
+    print('crescente')
+else:
+    print('Não está na ordem crescente')
